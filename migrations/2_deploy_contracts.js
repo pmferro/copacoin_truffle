be1a0da2472a4a8e2d6e0e334e575028f3142067
Pablo M. Ferro <pmferro@gmail.com>
2017-11-04 13:12:45 -0300

    Initial commit

---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,15 @@
+var ERC20Basic = artifacts.require("./ERC20Basic.sol");
+var Ownable = artifacts.require("./Ownable.sol");
+var Utils = artifacts.require("./Utils.sol");
+var CopaCoin = artifacts.require("./CopaCoin.sol");
+var Ballot = artifacts.require("RecuperarClase");
+
+module.exports = function(deployer) {
+  deployer.deploy(ERC20Basic);
+  //deployer.link(ERC20Basic, Ownable);
+  deployer.deploy(Ownable);
+  deployer.deploy(CopaCoin);
+  deployer.deploy(Utils);
+  //deployer.link(CopaCoin, Ballot);
+  deployer.deploy(Ballot);
+};
